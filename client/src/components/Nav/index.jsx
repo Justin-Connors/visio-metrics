35f40fe95c1ce38913c5265266e7e119f064e424
Justin <justinconnors@hotmail.ca>
2024-07-12 23:25:08 -0300

    added links to nav

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -4,18 +4,48 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  }
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  }
+
+  const handleSignupClick = () => {
+    navigate("/signup");
+  }
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="fixed">
           <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography 
+                variant="h6" 
+                component="div" 
+                sx={{ flexGrow: 1, cursor: "pointer" }}
+                onClick={handleLogoClick}
+            >
               VisioMetrics
             </Typography>
-            <Button color="inherit">Login</Button>
-            <Button color="inherit">Sign Up</Button>
+            <Button 
+              color="inherit"
+              onClick={handleLoginClick}
+            >
+              Login
+            </Button>
+            <Button 
+              color="inherit"
+              onClick={handleSignupClick}
+            >
+              Sign Up
+            </Button>
           </Toolbar>
         </AppBar>
       </Box>
